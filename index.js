62a2d688ed3a0b7b16c9e7c475fec8de9cc5032f
sole <sole@mozilla.com>
2015-05-06 15:09:06 +0100

    new version that also knows how to browserify-transforms

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var browserify = require('gulp-browserify');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var path = require('path');
 
 var buildDir = './build';
@@ -11,13 +11,36 @@ module.exports = function(gulp) {
 	gulp.task('build', ['build-js', 'build-html', 'build-data']);
 
 	gulp.task('build-js', function() {
-		return gulp.src('./demo/main.js')
-			.pipe(browserify({
-			}))
-			//.pipe(uglify())
-			.pipe(rename('bundle.js'))
+
+		// This is a "hack" to find if the top module is using browserify transforms as otherwise
+		// we are not using them and things remain untransformed which is not cool at all.
+		var transforms = [];
+		
+		try {
+			var packageJSON = require( '../../package.json' );
+			var hasTransforms = packageJSON.browserify !== undefined && packageJSON.browserify.transform !== undefined;
+
+			if(hasTransforms) {
+				transforms = packageJSON.browserify.transform;
+			}
+			console.log(packageJSON);
+		} catch(e) {
+			console.info('No browserify transforms found');
+		}
+
+		var b = browserify({
+			entries: './demo/main.js',
+			debug: true,
+			transform: transforms
+		});
+
+		return b.bundle()
+			.pipe(source('bundle.js'))
+			.pipe(buffer())
 			.pipe(gulp.dest(buildDir));
+
 	});
+
 
 	gulp.task('build-html', function() {
 		return gulp.src('demo/index.html')
@@ -30,7 +53,7 @@ module.exports = function(gulp) {
 	});
 
 	gulp.task('watch', function() {
-		gulp.watch(['demo/**/*', '*.js', 'src/**/*'], ['build']);
+		gulp.watch(['demo/**/*', 'index.js', 'src/**/*'], ['build']);
 	});
 
 	gulp.task('default', ['build', 'watch']);
